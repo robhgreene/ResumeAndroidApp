73c01e60815c591f972a35fd07581fe369e542b5
Robert H Greene <robhgreene@gmail.com>
2022-09-01 01:06:57 -0700

    Finish Resume App

--- app/src/main/java/com/example/resume/MainActivity.java
+++ app/src/main/java/com/example/resume/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.resume;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     // this is how we link an action to switching to another activity
     Button workHistoryButton;
+    Button callButton;
+    Button emailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         workHistoryButton = (Button) findViewById(R.id.workHistoryButton);
+        callButton = (Button) findViewById(R.id.callRobButton);
+        emailButton = (Button) findViewById(R.id.emailRobButton);
 
         workHistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +30,27 @@ public class MainActivity extends AppCompatActivity {
                 // this is where we will use intent, such as moving from one activity to another
                 Intent goToTheOtherActivity = new Intent(getApplicationContext(), WorkHistoryActivity.class);
                 startActivity(goToTheOtherActivity);
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri myPhoneNumber = Uri.parse("tel:18009766447");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, myPhoneNumber);
+                startActivity(callIntent);
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"robIsGreat@hotmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Rob's Resume");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "I really like your resume");
+                startActivity(emailIntent);
             }
         });
     }
